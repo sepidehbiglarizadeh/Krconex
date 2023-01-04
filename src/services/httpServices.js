0ec78b159af1a5f224f2f3ab1e74452a8fa0dc31
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-04 16:51:25 +0330

    change endpoint

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL ="http://localhost:3001";
+axios.defaults.baseURL ="https://my-json-server.typicode.com/sepidehbiglarizadeh/Krconex";
 
 const http = {
   get: axios.get,
