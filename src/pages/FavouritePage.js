dd07bf2e1721323d00b28293e3e19e5de7f10d7e
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-04 14:44:52 +0330

    display favourite products on the Favourites Page

--- src/pages/FavouritePage.js
+++ src/pages/FavouritePage.js
@@ -1,5 +1,34 @@
+import { render } from "react-dom";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Product from "../components/Product/Product";
+
 const FavouritePage = () => {
-    return ( <div>This is FavouritePage</div> );
-}
- 
+  const fav = useSelector((state) => state.fav.favProducts);
+
+  let renderValue;
+
+  if (fav.length) {
+    renderValue = fav.map((product) => (
+      <Link to={`/product/${product.id}`}>
+        <Product key={product.id} product={product} />
+      </Link>
+    ));
+  } else {
+    renderValue = (
+      <p className="text-center col-span-4 mt-8">Your WishList is Empty!!</p>
+    );
+  }
+
+  return (
+    <section
+      className={
+        "grid xs:gap-x-4 gap-y-8 min-h-[50vh] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-4"
+      }
+    >
+      {renderValue}
+    </section>
+  );
+};
+
 export default FavouritePage;
